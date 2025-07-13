2eaae8252ccd5e03c11cb18d9bcef9accdad926d
Rahul Singh <docoderahul@gmail.com>
2025-07-14 00:31:06 +0530

    Add WhyChooseUS component and StickyScroll functionality to enhance user engagement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUS from "@/components/WhyChooseUS";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <FeaturedCourses />
+      <WhyChooseUS />
     </>
   );
 }
