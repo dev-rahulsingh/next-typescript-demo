5160cdba883620bab08a50e33e6585378cdb54bf
Rahul Singh <docoderahul@gmail.com>
2025-07-13 17:21:06 +0530

    Add HeroSection component with spotlight animation and button functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl text-center font-bold">Home Page Bolte</h1>
+      <HeroSection />
     </>
   );
 }
